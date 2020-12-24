0acd64d10f332c10a56153a98dfaa0df4cb67d65
Alex Velasco <cristianvc-99@hotmail.com>
2020-12-24 03:39:49 -0600

    designing register screen

--- EatsApp/App/Login/LogInViewController.swift
+++ EatsApp/App/Login/LogInViewController.swift
@@ -132,15 +132,6 @@ class LogInViewController: UIViewController {
         phoneNumberTextField.endEditing(true)
       }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     //funcion del phoneNumberTextField
     private func getCustomTextFieldInputAccessoryView(with items: [UIBarButtonItem]) -> UIToolbar {
         let toolbar: UIToolbar = UIToolbar()
---
+++ EatsApp/App/Register/RegisterViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RegisterViewController.swift
+//  EatsApp
+//
+//  Created by Alejandro Velasco on 24/12/20.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var nameTexfield: UITextField!
+    @IBOutlet weak var phoneTextfield: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerButton(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameTexfield.layer.borderWidth = 0.5
+        nameTexfield.layer.borderColor = UIColor(named: "Amarillo")?.cgColor
+        nameTexfield.layer.cornerRadius = 5
+        
+        phoneTextfield.layer.borderWidth = 0.5
+        phoneTextfield.layer.borderColor = UIColor(named: "Amarillo")?.cgColor
+        phoneTextfield.layer.cornerRadius = 5
+        
+        emailTextfield.layer.borderWidth = 0.5
+        emailTextfield.layer.borderColor = UIColor(named: "Amarillo")?.cgColor
+        emailTextfield.layer.cornerRadius = 5
+        
+        passwordTextfield.layer.borderWidth = 0.5
+        passwordTextfield.layer.borderColor = UIColor(named: "Amarillo")?.cgColor
+        passwordTextfield.layer.cornerRadius = 5
+        
+        
+    }
+    
+
+    
+}
