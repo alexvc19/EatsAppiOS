3826dcb81b38bdfe1925d2d344e3135676aaf696
Alex Velasco <cristianvc-99@hotmail.com>
2020-12-24 19:28:27 -0600

    Added scroll home and starting cardView.xib

---
+++ EatsApp/App/Home/CardUIView.swift
@@ -0,0 +1,19 @@
+//
+//  CardUIView.swift
+//  EatsApp
+//
+//  Created by Alejandro Velasco on 24/12/20.
+//
+
+import UIKit
+
+class CardUIView: UIView {
+
+    var view:UIView!
+    
+    @IBOutlet weak var cardImageview: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var estimateTimeLabel: UILabel!
+    
+   
+}
