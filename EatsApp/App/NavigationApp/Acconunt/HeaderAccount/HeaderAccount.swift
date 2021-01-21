bad79998b6b7e286977251e14157ba5b11eb6a49
alexvc19 <cristianvc-99@hotmail.com>
2021-01-21 01:55:36 -0600

    Update AccountScreen

--- EatsApp/App/NavigationApp/Acconunt/HeaderAccount/HeaderAccount.swift
+++ EatsApp/App/NavigationApp/Acconunt/HeaderAccount/HeaderAccount.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class HeaderAccount: UIView {
-    
+        
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userPoitsLabel: UILabel!
 
     override init(frame: CGRect){
         super.init(frame: frame)
@@ -22,7 +25,13 @@ class HeaderAccount: UIView {
     
     private func commonInit(){
         let viewFromXib = Bundle.main.loadNibNamed("HeaderAccount", owner: self, options: nil)![0] as! UIView
-    
+ 
+        userImageView.image = UIImage(named: "default")
+        userImageView.layer.cornerRadius = (userImageView.frame.size.width ) / 2
+        userImageView.clipsToBounds = true
+        userImageView.layer.borderWidth = 1.0
+        userImageView.layer.borderColor = UIColor(named: "Rojo")?.cgColor
+        
         viewFromXib.frame = self.bounds
         addSubview(viewFromXib)
         
