2492bd013da0206d6b81679ae616197db0f24868
alexvc19 <cristianvc-99@hotmail.com>
2021-01-21 17:32:02 -0600

    PayMethods screen added

--- EatsApp/App/NavigationApp/Acconunt/Options/PayMethods/PayMethodsViewController.swift
+++ EatsApp/App/NavigationApp/Acconunt/Options/PayMethods/PayMethodsViewController.swift
@@ -7,23 +7,52 @@
 
 import UIKit
 
-class PayMethodsViewController: UIViewController {
-
+class PayMethodsViewController: UIViewController, UITableViewDataSource, UITabBarDelegate{
+    
+    @IBOutlet weak var paymentsTableView: UITableView!
+    
+    let methodsPayNames = [
+        "Efectivo",
+        "Paypal",
+        "Tarjeta de Credito/Debito"
+    ]
+    let methodsPayIcons = [
+        "dollarsign.circle.fill",
+        "dollarsign.square.fill",
+        "creditcard.fill"
+    ]
+    
+    //MARK: - TableView Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return methodsPayNames.count
+    }
+    //MARK: - TableView DataSorce
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+      let cell = paymentsTableView.dequeueReusableCell(withIdentifier: "methodpay", for: indexPath) as! PaymentMethodTableViewCell
+        
+        cell.payMethodLabel.text = methodsPayNames[indexPath.row]
+        cell.payMethodImageView.image = UIImage(systemName: methodsPayIcons[indexPath.row])
+        
+        return cell
+    }
+    
+    //MARK:- VIEWDIDLOAD
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 40.0))
+        
+        let titleStoryeboard = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 25))
+        titleStoryeboard.text = "Metodos de pago"
+        titleStoryeboard.textAlignment = .center
+        titleStoryeboard.tintColor = .black
+        titleStoryeboard.font = UIFont(name: "Helvetica-Bold", size: 21)
+        titleStoryeboard.center = header.center
+        
+        header.addSubview(titleStoryeboard)
+        
+        paymentsTableView.tableHeaderView = header
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
