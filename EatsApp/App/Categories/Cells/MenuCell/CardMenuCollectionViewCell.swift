82ff5102674cc7b6342a8f5e02db5f343af5605c
alexvc19 <cristianvc-99@hotmail.com>
2021-01-27 21:06:20 -0600

    menu collectionview add at card restaurant on categories screen

---
+++ EatsApp/App/Categories/Cells/MenuCell/CardMenuCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MenuCollectionViewCell.swift
+//  EatsApp
+//
+//  Created by Alejandro Velasco on 27/01/21.
+//
+
+import UIKit
+
+class CardMenuCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var menuPhotoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- EatsApp/App/Categories/Cells/RestaurantsTableViewCell.swift
+++ EatsApp/App/Categories/Cells/RestaurantsTableViewCell.swift
@@ -8,16 +8,22 @@
 import UIKit
 
 class RestaurantsTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var restaurantNameLabel: UILabel!
     @IBOutlet weak var deliveryCost: UILabel!
     @IBOutlet weak var estimateTimeLabel: UILabel!
     
-    @IBOutlet weak var cardView: CardViewCell!
+    @IBOutlet weak var menuCardCollectionView:UICollectionView!
+    
+    var cellNib = "cardMenuCell"
     
     override func awakeFromNib() {
         super.awakeFromNib()
-       
+        
+        menuCardCollectionView.register(UINib(nibName: "CardMenuCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellNib)
+        self.menuCardCollectionView.dataSource = self
+        self.menuCardCollectionView.delegate = self
     }
 
     override func layoutSubviews() {
@@ -31,5 +37,21 @@ class RestaurantsTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+}
+extension RestaurantsTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = menuCardCollectionView.dequeueReusableCell(withReuseIdentifier: cellNib, for: indexPath) as? CardMenuCollectionViewCell else {
+            fatalError()
+        }
+        return cell
+    }
+    
     
 }
