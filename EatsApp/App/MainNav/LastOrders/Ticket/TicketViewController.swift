4763558b6a646fa4d843902540d196ee26a02120
alexvc19 <cristianvc-99@hotmail.com>
2021-02-06 10:54:37 -0600

    Working on ticket screen

--- EatsApp/App/MainNav/LastOrders/Ticket/TicketViewController.swift
+++ EatsApp/App/MainNav/LastOrders/Ticket/TicketViewController.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class TicketViewController: UIViewController {
+    
+    @IBOutlet weak var helpButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        helpButton.layer.borderWidth = 0.5
+        helpButton.layer.borderColor = UIColor(named: "Rojo")?.cgColor
+        helpButton.layer.cornerRadius = 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
