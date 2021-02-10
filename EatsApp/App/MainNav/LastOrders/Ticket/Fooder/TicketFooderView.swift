3eb5b5b3926bfa439332eedd6cb5573c2df19598
alexvc19 <cristianvc-99@hotmail.com>
2021-02-10 16:31:46 -0600

    Reusable starts for cards

--- EatsApp/App/MainNav/LastOrders/Ticket/Fooder/TicketFooderView.swift
+++ EatsApp/App/MainNav/LastOrders/Ticket/Fooder/TicketFooderView.swift
@@ -9,55 +9,13 @@ import UIKit
 
 class TicketFooderView: UIView {
 
-    @IBOutlet weak var star1: UIButton!
-    @IBOutlet weak var star2: UIButton!
-    @IBOutlet weak var star3: UIButton!
-    @IBOutlet weak var star4: UIButton!
-    @IBOutlet weak var star5: UIButton!
+   
     
     @IBOutlet weak var textField: UITextView!
     
     @IBOutlet weak var helpButton: UIButton!
     @IBOutlet weak var starsStack: UIStackView! 
-    let starIcon = "star.fill"
-    
-    @IBAction func pressStar1(_ sender: Any) {
-        star1.setImage(UIImage(systemName: starIcon), for: .normal)
-        star2.setImage(UIImage(systemName: "star"), for: .normal)
-        star3.setImage(UIImage(systemName: "star"), for: .normal)
-        star4.setImage(UIImage(systemName: "star"), for: .normal)
-        star5.setImage(UIImage(systemName: "star"), for: .normal)
-    }
-    @IBAction func pressStar2(_ sender: Any) {
-        star1.setImage(UIImage(systemName: starIcon), for: .normal)
-        star2.setImage(UIImage(systemName: starIcon), for: .normal)
-        star3.setImage(UIImage(systemName: "star"), for: .normal)
-        star4.setImage(UIImage(systemName: "star"), for: .normal)
-        star5.setImage(UIImage(systemName: "star"), for: .normal)
         
-    }
-    @IBAction func pressStar3(_ sender: Any) {
-        star1.setImage(UIImage(systemName: starIcon), for: .normal)
-        star2.setImage(UIImage(systemName: starIcon), for: .normal)
-        star3.setImage(UIImage(systemName: starIcon), for: .normal)
-        star4.setImage(UIImage(systemName: "star"), for: .normal)
-        star5.setImage(UIImage(systemName: "star"), for: .normal)
-    }
-    @IBAction func pressStar4(_ sender: Any) {
-        star1.setImage(UIImage(systemName: starIcon), for: .normal)
-        star2.setImage(UIImage(systemName: starIcon), for: .normal)
-        star3.setImage(UIImage(systemName: starIcon), for: .normal)
-        star4.setImage(UIImage(systemName: starIcon), for: .normal)
-        star5.setImage(UIImage(systemName: "star"), for: .normal)
-    }
-    @IBAction func pressStar5(_ sender: Any) {
-        star1.setImage(UIImage(systemName: starIcon), for: .normal)
-        star2.setImage(UIImage(systemName: starIcon), for: .normal)
-        star3.setImage(UIImage(systemName: starIcon), for: .normal)
-        star4.setImage(UIImage(systemName: starIcon), for: .normal)
-        star5.setImage(UIImage(systemName: starIcon), for: .normal)
-    }
-    
     override init(frame: CGRect){
         super.init(frame: frame)
         commonInit()
